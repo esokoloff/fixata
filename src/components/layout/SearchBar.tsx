aa023a2f4a3543beb32b12b8c75b42b928af65ed
Emiliyan Sokolov <emiliyan.sokolov@gmail.com>
2020-10-28 17:17:43 +0200

    Add placeholder to the searchbar

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -6,7 +6,7 @@ const SearchBar: React.FC = () => {
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input type="search" id="search" />
+            <input type="search" id="search" placeholder="Search Logs..." />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
